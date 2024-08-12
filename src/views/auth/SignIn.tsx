481eb2e04b901ffe013359c77cbfa2a163c11a02
Sebastian Ramírez Ramírez <86761419+tevyramirez@users.noreply.github.com>
2024-08-12 12:06:51 -0400

    Update SignIn.tsx
    Actualizar Signin

--- src/views/auth/SignIn.tsx
+++ src/views/auth/SignIn.tsx
@@ -25,12 +25,11 @@ const SignIn: React.FC = () => {
 
       if (response.ok) {
         const data = await response.json();
+        localStorage.setItem('access_token', data.access);  // Guardar el token
         login(data.access);
-        console.log("se logea")
-        // Redirigir al usuario a la página deseada después del inicio de sesión
-        // Ejemplo: redirige a la página de dashboard
-        window.location.href = '/admin';
-
+        console.log("Login successful");
+        
+        window.location.href = '/admin'; // Redirigir al dashboard
       } else {
         const data = await response.json();
         setError(data.detail || 'Ocurrió un error, intenta nuevamente');
